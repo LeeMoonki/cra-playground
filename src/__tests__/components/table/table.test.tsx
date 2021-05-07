addc40df63d3c500fc55e133f636ca39a841c43b
Junep <prepared@inflearn.com>
2021-05-07 22:39:35 +0900

    row 추가 1. 데이터에 상관없이 데이터로 전달된 행의 갯수만큼 tr을 생성하는지 확인 2. 순서는 먼저 사용하길 원하는 환경을 구성해서 테스트를 작성하고 그 테스트에 맞춰 컴포넌트를 만든다.

--- src/__tests__/components/table/table.test.tsx
+++ src/__tests__/components/table/table.test.tsx
@@ -2,9 +2,21 @@ import { render } from '@testing-library/react';
 import Table from '../../../components/table';
 
 describe('Table 컴포넌트', () => {
-  it('렌더링을 확인합니다.', () => {
-    const { container } = render(<Table />);
+  it('기본 렌더링을 확인합니다.', () => {
+    const header = [
+      { name: 'col1', key: 'col1' },
+      { name: 'col2', key: 'col2' },
+      { name: 'col3', key: 'col3' },
+      { name: 'col4', key: 'col4' },
+    ];
+    const rows = [
+      { col1: 'col1-row1', col2: 'col2-row1', col3: 'col3-row1', col4: 'col4-row1' },
+      { col1: 'col1-row2', col2: 'col2-row2', col3: 'col3-row2', col4: 'col4-row2' },
+      { col1: 'col1-row3', col2: 'col2-row3', col3: 'col3-row3', col4: 'col4-row3' },
+    ];
 
-    expect(container.querySelector('table')).toBeTruthy();
+    const { container } = render(<Table header={header} rows={rows} />);
+
+    expect(container.querySelectorAll('table tr').length).toBe(rows.length);
   });
 });
--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,5 +1,27 @@
-function Table() {
-  return <table></table>;
+export interface BaseTableHeader {
+  name: string;
+  key: string;
+}
+
+function Table<TableHeader extends BaseTableHeader = BaseTableHeader>({
+  header,
+  rows,
+}: {
+  header: TableHeader[];
+  rows: {
+    [key in typeof header[number]['key']]: string;
+  }[];
+}) {
+  return (
+    <table>
+      <tbody>
+        {rows.length > 0 &&
+          rows.map((row, index) => {
+            return <tr key={index}></tr>;
+          })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
