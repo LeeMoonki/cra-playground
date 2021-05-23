4fb295f988da46fddf179a4b408f73d749e25a57
Moonki Lee <shinbaek89@gmail.com>
2021-05-23 09:11:24 +0900

    Counter 기본 동작

--- src/__tests__/components/counter/counter.test.tsx
+++ src/__tests__/components/counter/counter.test.tsx
@@ -12,4 +12,41 @@ describe('Counter', () => {
 
     expect($count.textContent).toBe('0');
   });
+
+  it('증가 버튼을 클릭하면 값이 한 단위 증가합니다.', () => {
+    render(<Counter />);
+
+    const $count = screen.queryByTestId('count');
+    const $buttonIncrease = screen.queryByRole('button', { name: '증가' });
+
+    fireEvent.click($buttonIncrease);
+
+    expect($count.textContent).toBe('1');
+  });
+
+  it('감소 버튼을 클릭하면 값이 한 단위 감소합니다.', () => {
+    render(<Counter />);
+
+    const $count = screen.queryByTestId('count');
+    const $buttonDecrease = screen.queryByRole('button', { name: '감소' });
+
+    fireEvent.click($buttonDecrease);
+
+    expect($count.textContent).toBe('-1');
+  });
+
+  it('초기화 버튼을 클릭하면 값을 초기화합니다.', () => {
+    render(<Counter />);
+
+    const $count = screen.queryByTestId('count');
+    const $buttonIncrease = screen.queryByRole('button', { name: '증가' });
+    const $buttonReset = screen.queryByRole('button', { name: '초기화' });
+
+    fireEvent.click($buttonIncrease);
+    fireEvent.click($buttonIncrease);
+
+    fireEvent.click($buttonReset);
+
+    expect($count.textContent).toBe('0');
+  });
 });
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -6,6 +6,11 @@ function Counter() {
   return (
     <section>
       <span data-testid="count">{count}</span>
+      <section>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        <button onClick={() => setCount(0)}>초기화</button>
+      </section>
     </section>
   );
 }
