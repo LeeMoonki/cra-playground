8e83663069b878b44d2159c6c895f62fa0f48a12
Moonki Lee <shinbaek89@gmail.com>
2021-05-23 09:18:28 +0900

    컴포넌트 세분화

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,16 +1,32 @@
 import { useState } from 'react';
 
+interface ControllersProps {
+  onIncrease?: () => void;
+  onDescrease?: () => void;
+  onReset?: () => void;
+}
+
+export function Controllers({ onIncrease, onDescrease, onReset }: ControllersProps) {
+  return (
+    <section>
+      <button onClick={() => onDescrease && onDescrease()}>감소</button>
+      <button onClick={() => onIncrease && onIncrease()}>증가</button>
+      <button onClick={() => onReset && onReset()}>초기화</button>
+    </section>
+  );
+}
+
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
     <section>
       <span data-testid="count">{count}</span>
-      <section>
-        <button onClick={() => setCount(count - 1)}>감소</button>
-        <button onClick={() => setCount(count + 1)}>증가</button>
-        <button onClick={() => setCount(0)}>초기화</button>
-      </section>
+      <Controllers
+        onIncrease={() => setCount(count + 1)}
+        onDescrease={() => setCount(count - 1)}
+        onReset={() => setCount(0)}
+      />
     </section>
   );
 }
