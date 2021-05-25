c1cd190c445ef2685b579693bd99670229cf4a55
Junep <prepared@inflearn.com>
2021-05-25 09:31:32 +0900

    new: hooks 디렉터리 및 useMounted

---
+++ src/hooks/useMounted.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+function useMounted() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted;
+}
+
+export default useMounted;
