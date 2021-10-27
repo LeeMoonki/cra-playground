a705a839741d2c8027b19d94756eab9c6ec5fc7d
Junep <prepared@inflearn.com>
2021-10-27 21:40:15 +0900

    mobx private text 시도

--- src/pages/MobxGround/Ground1.tsx
+++ src/pages/MobxGround/Ground1.tsx
@@ -34,17 +34,31 @@ const CountPanel = observer(() => {
 });
 
 const Text = observer(() => {
-  const { textStore } = useInject();
+  const { textStore, privateText, setPrivateText, computedPrivateText } = useInject();
   const { userStore } = useInjectRoot();
   console.log('Text render');
   return (
     <div style={{ backgroundColor: '#0f0' }}>
       <span>{textStore.text}</span>
+      <br />
       <span>{userStore.isLogin ? 'login' : 'logout'}</span>
+      <br />
+      <span>Private Text: {privateText}</span>
+      <br />
+      <span>Computed Text1: {computedPrivateText}</span>
+      <br />
       <input
+        placeholder="text"
         type="text"
         onChange={({ target: { value } }) => {
           textStore.setText(value);
+        }}
+      />
+      <input
+        placeholder="private text"
+        type="text"
+        onChange={({ target: { value } }) => {
+          setPrivateText(value);
         }}
       />
     </div>
--- src/pages/MobxGround/PageStore.ts
+++ src/pages/MobxGround/PageStore.ts
@@ -1,3 +1,4 @@
+import { action, computed, makeObservable, observable } from 'mobx';
 import CounterStore from './model/Counter';
 import TextStore from './model/Text';
 
@@ -5,8 +6,26 @@ export default class PageStore {
   counterStore: CounterStore;
   textStore: TextStore;
 
+  privateText: string;
+
   constructor() {
+    makeObservable(this, {
+      privateText: observable,
+      computedPrivateText: computed,
+      setPrivateText: action,
+    });
     this.counterStore = new CounterStore(this);
     this.textStore = new TextStore(this);
+
+    this.privateText = '';
   }
+
+  get computedPrivateText() {
+    return this.privateText + ' :)';
+  }
+
+  setPrivateText = (text: string) => {
+    // this._privateText = text;
+    this.privateText = text;
+  };
 }
